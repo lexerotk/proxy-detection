b5ad68b9f3f0e73a94c0eab3a1b759bfde36f4b1
lexerotk <lexerotk@gmail.com>
2025-04-26 18:28:01 +0300

    Update qualityscore.js

--- api/qualityscore.js
+++ api/qualityscore.js
@@ -1,4 +1,8 @@
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  
   const { ip } = req.query;
 
   if (!ip) {
