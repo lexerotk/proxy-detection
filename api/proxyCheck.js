c3949a15037fd2eec9362e1f360717f8360372be
lexerotk <lexerotk@gmail.com>
2025-05-29 19:35:29 +0300

    Error log message change

--- api/proxyCheck.js
+++ api/proxyCheck.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
 
     res.status(200).json(data);
   } catch (error) {
-    console.error('VPN Check API Error:', error);
+    console.error('API ERR:', error);
     res.status(500).json({ error: 'Internal server error.' });
   }
 }
